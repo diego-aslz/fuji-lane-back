92f95571ac74c2c8ede3e949fee4b99f81a9074a
Diego Selzlein <diegoselzlein@gmail.com>
2018-11-04 08:56:18 -0500

    Fixed JSON for Unit.

--- flentities/unit.go
+++ flentities/unit.go
@@ -1,24 +1,27 @@
 package flentities
 
-import "github.com/jinzhu/gorm"
+import "time"
 
 // Unit represents a type of rentable unit inside a Property. An individual property can have multiple units at
 // different prices for rent
 type Unit struct {
-	gorm.Model
-	PropertyID             *uint     `json:"-"`
-	Property               *Property `json:"-"`
-	Name                   string    `json:"name"`
-	Bedrooms               int       `json:"bedrooms"`
-	SizeM2                 *int      `json:"sizeM2"`
-	MaxOccupancy           *int      `json:"maxOccupancy"`
-	Count                  int       `json:"count"`
-	BasePriceCents         *int      `json:"basePriceCents"`
-	OneNightPriceCents     *int      `json:"oneNightPriceCents"`
-	OneWeekPriceCents      *int      `json:"oneWeekPriceCents"`
-	ThreeMonthsPriceCents  *int      `json:"threeMonthsPriceCents"`
-	SixMonthsPriceCents    *int      `json:"sixMonthsPriceCents"`
-	TwelveMonthsPriceCents *int      `json:"twelveMonthsPriceCents"`
-	FloorPlanImageID       *uint     `json:"-"`
-	FloorPlanImage         *Image    `json:"floorPlanImage"`
+	ID                     uint       `gorm:"primary_key" json:"id"`
+	CreatedAt              time.Time  `json:"-"`
+	UpdatedAt              time.Time  `json:"-"`
+	DeletedAt              *time.Time `sql:"index" json:"-"`
+	PropertyID             *uint      `json:"propertyID"`
+	Property               *Property  `json:"-"`
+	Name                   string     `json:"name"`
+	Bedrooms               int        `json:"bedrooms"`
+	SizeM2                 *int       `json:"sizeM2"`
+	MaxOccupancy           *int       `json:"maxOccupancy"`
+	Count                  int        `json:"count"`
+	BasePriceCents         *int       `json:"basePriceCents"`
+	OneNightPriceCents     *int       `json:"oneNightPriceCents"`
+	OneWeekPriceCents      *int       `json:"oneWeekPriceCents"`
+	ThreeMonthsPriceCents  *int       `json:"threeMonthsPriceCents"`
+	SixMonthsPriceCents    *int       `json:"sixMonthsPriceCents"`
+	TwelveMonthsPriceCents *int       `json:"twelveMonthsPriceCents"`
+	FloorPlanImageID       *uint      `json:"-"`
+	FloorPlanImage         *Image     `json:"floorPlanImage"`
 }
