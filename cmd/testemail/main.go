1e4388a119c8bdef1181b46b4671238cbd2957e1
Diego Selzlein <diegoselzlein@gmail.com>
2019-02-14 09:38:57 -0200

    Making testemail command send a booking email.

--- cmd/testemail/main.go
+++ cmd/testemail/main.go
@@ -1,24 +1,43 @@
 package main
 
 import (
-	"net/textproto"
-
 	"github.com/nerde/fuji-lane-back/flconfig"
+	"github.com/nerde/fuji-lane-back/flemail"
+	"github.com/nerde/fuji-lane-back/flentities"
 	"github.com/nerde/fuji-lane-back/flservices"
-
-	"github.com/jordan-wright/email"
 )
 
 func main() {
-	e := &email.Email{
-		To:      []string{"diegoselzlein@gmail.com"},
-		Subject: "Test Email",
-		Text:    []byte("Text Body is, of course, supported!"),
-		HTML:    []byte("<h1>Fancy HTML is supported, too!</h1>"),
-		Headers: textproto.MIMEHeader{},
+	flconfig.LoadConfiguration()
+
+	pName := "Awesome Property"
+	uName := "Requestor"
+	checkIn, _ := flentities.ParseDate("2019-02-01")
+	checkOut, _ := flentities.ParseDate("2019-02-08")
+	b := &flentities.Booking{
+		User: &flentities.User{
+			Name:  &uName,
+			Email: "requestor@fujilane.com",
+		},
+		Unit: &flentities.Unit{
+			Name:     "Standard Apartment",
+			Property: &flentities.Property{Name: &pName},
+		},
+		CheckIn:       checkIn,
+		CheckOut:      checkOut,
+		Nights:        7,
+		PerNightCents: 10000,
+		TotalCents:    70000,
 	}
 
-	flconfig.LoadConfiguration()
+	owner := &flentities.User{
+		Email: "owner@fujilane.com",
+	}
+
+	e, er := flemail.BookingCreated(b, owner)
+	if er != nil {
+		panic(er)
+	}
 
 	if err := flservices.NewSMTPMailer().Send(e); err != nil {
 		panic(err)
